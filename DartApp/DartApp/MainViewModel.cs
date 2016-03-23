9df862a41f582e121b7fa183af61dfe9867433a3
Daniel Leitner <dmcleite@gmail.com>
2016-03-23 23:47:51 +0100

    added viewmodel. added resource dictionary, added styling file. implemented mainWindow

---
+++ DartApp/DartApp/MainViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System.Windows.Input;
+using Base;
+
+namespace DartApp
+{
+	public class MainViewModel : ViewModelBase
+	{
+		private string state;
+		private RelayCommand holidayCommand;
+		private RelayCommand trainingCommand;
+		private RelayCommand vdsvCommand;
+		public MainViewModel()
+		{
+			
+		}
+
+		public string State
+		{
+			get
+			{
+				return this.state;
+			}
+			set
+			{
+				this.state = value;
+				OnPropertyChanged("State");
+			}
+		}
+
+		public ICommand HolidayCommand
+		{
+			get
+			{
+				return this.holidayCommand ?? (this.holidayCommand = new RelayCommand(
+					param => OpenHoliday()
+					));
+			}
+		}
+
+		private void OpenHoliday()
+		{
+			
+		}
+
+		public ICommand TrainingCommand
+		{
+			get
+			{
+				return this.trainingCommand ?? (this.trainingCommand = new RelayCommand(
+					param => OpenTraining(),
+					param => CanOpenTraining()
+					));
+			}
+		}
+
+		private void OpenTraining()
+		{
+
+		}
+
+		private bool CanOpenTraining()
+		{
+			return false;
+		}
+
+		public ICommand VdsvCommand
+		{
+			get
+			{
+				return this.vdsvCommand ?? (this.vdsvCommand = new RelayCommand(
+					param => OpenVdsv(),
+					param => CanOpenVdsv()
+					));
+			}
+		}
+
+		private void OpenVdsv()
+		{
+
+		}
+
+		private bool CanOpenVdsv()
+		{
+			return false;
+		}
+	}
+}
--- DartApp/DartApp/MainWindow.xaml.cs
+++ DartApp/DartApp/MainWindow.xaml.cs
@@ -15,14 +15,16 @@ using System.Windows.Shapes;
 
 namespace DartApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			var viewModel = new MainViewModel();
+			this.DataContext = viewModel;
+			InitializeComponent();
+		}
+	}
 }
